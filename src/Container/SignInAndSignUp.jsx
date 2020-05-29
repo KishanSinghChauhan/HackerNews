2ca3068eb61939266f39574bbb1d410daea2b4e3
kishansinghchauhan <kishancoer7071@gmail.com>
2020-05-29 18:48:14 +0530

    ++

--- src/Container/SignInAndSignUp.jsx
+++ src/Container/SignInAndSignUp.jsx
@@ -5,10 +5,17 @@ import SignUp from '../Components/sign-up';
 import './styles/SignInAndSingUp.scss'
 
 const SignInAndSignUpPage = () => (
-  <div className='sign-in-and-sign-up'>
-    <SignIn />
-    <SignUp />
-  </div>
+  <>
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+      <SignUp />
+    </div>
+    <div className="msg">
+      <h4>SignIn and SignUp with e-mail will be add soon.</h4>
+      <h4 className="ml-5">Yet you can use signin with google.</h4>
+    </div>
+    
+  </>
 );
 
 export default SignInAndSignUpPage;
